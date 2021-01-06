751a41be515eb49b0f9d01f4f316890318eb7900
parthkakadia <parthkakadia@hotmail.com>
2021-01-07 01:36:28 +0530

    Service level error handling

--- src/api/dao/base.dao.ts
+++ src/api/dao/base.dao.ts
@@ -1,33 +1,30 @@
 import { Model } from "mongoose";
-
+import { ErrorHandler } from "../common";
 class BaseDAO {
-
-  private model:Model<any>;
+  private model: Model<any>;
 
   constructor(model: Model<any>) {
     this.model = model;
   }
 
-  async create(obj:any){
-   return await this.model.create(obj);
+  async create(obj: any) {
+    return await this.model.create(obj);
+  }
+  async update(query: any, obj: any) {
+    return await this.model.updateOne(query, obj);
   }
 
-  async update(query:any,obj:any){
-    return await this.model.updateOne(query,obj);
-  }
-
-  async delete(id:any){
+  async delete(id: any) {
     return await this.model.deleteOne(id);
   }
-  
-  async getAll(){
+
+  async getAll() {
     return await this.model.find({});
   }
 
-  async getByID(id:any){
+  async getByID(id: any) {
     return await this.model.findOne(id);
   }
-
 }
 
 export default BaseDAO;
--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,21 @@ import app from "./app";
 import { logger } from "./utils";
 import { initDB } from "./db";
 
-
 let server;
+process.on("uncaughtException", (err) => {
+  logger.error(err.name, err.message);
+  logger.error("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  process.exit(1);
+});
 initDB().then(() => {
   server = app.listen(app.get("port"), () => {
-    logger.log(
-      "info",
+    logger.info(
       "App is running at http://localhost:" +
         app.get("port") +
         " in " +
         app.get("env") +
         " mode"
     );
-    logger.log("info", "Press CTRL-C to stop\n");
   });
 });
 
